c959be1007ddc689eb5484388262d35bc5a40e48
amogha r <amoghar23@gmail.com>
2025-02-22 13:18:58 +0530

    added vercel.json

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
+    emptyOutDir: true, // Ensure the directory is created if it doesn't exist
   },
-  server:{
-    port: 3001
-  }
+  server: {
+    port: 3001,
+  },
 });
