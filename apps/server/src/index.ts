46b61624484b3dfe54876f2c3294a1729432b301
amogha r <amoghar23@gmail.com>
2025-02-22 14:18:28 +0530

    cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,15 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: FRONTEND_URL, // Your frontend URL
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-    exposedHeaders: ["set-cookie"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
